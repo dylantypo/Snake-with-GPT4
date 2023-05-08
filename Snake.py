4ef1db80b400d5d272c66dcac1f2e30ec4ea24e8
dylantypo <dylanp@vt.edu>
2023-05-08 17:55:07 -0400

    Removed ChatGPT message from code since I created the read me

--- Snake.py
+++ Snake.py
@@ -1,22 +1,3 @@
-# # Snake Game Created with ChatGPT-4
-
-# ### Message from ChatGPT
-# This code provides a basic snake game with a simple retro style. 
-# You can run the code, and it will display a snake game window with a white background, a green snake, and red food. 
-# Use the arrow keys to control the snake. 
-# If you want to modify the game's appearance or behavior further, you can do so by changing the constants, colors, or other parts of the code.
-
-# Some ideas for expanding the game or enhancing its retro style:
-
-# 1. ~~Add a score display that shows the player's current score, which could be based on the snake's length.~~
-# 2. ~~Add a game over screen with the option to restart the game.~~
-# 3. ~~Implement different levels of difficulty, such as faster snake movement~~ or additional obstacles.
-# 4. Add sounds or music to create a more immersive experience.
-# 5. Create a retro-style pixel art for the snake, food, and background.
-# 6. Customize the game window's appearance, such as adding a retro border or ~~using a pixelated font.~~
-
-# Feel free to experiment and modify the code as needed to create the snake game you envision.
-
 #### Imports
 import pygame
 import pygame.gfxdraw
